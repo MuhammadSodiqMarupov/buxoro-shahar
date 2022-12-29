3e189715184b97440c76491087119381b3e3f462
MuhammadYusuf <marupovmuhammadyusuf988@gmail.com>
2022-12-30 00:15:42 +0500

    qarib full holatda

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Section2 from "./components/Section2/Section2";
 import Section3 from "./components/Section3/Section3";
 import Section4 from "./components/Section4/Section4";
-import FOTO_VIDEO_GALERY from "./components/FOTO_VIDEO_GALERY/FOTO_VIDEO_GALERY";
+// import FOTO_VIDEO_GALERY from "./components/FOTO_VIDEO_GALERY/FOTO_VIDEO_GALERY";
 import Section5 from "./components/Section5/Section5";
 import Footer from "./components/Footer/Footer";
 import {Routes,Route} from "react-router-dom";
@@ -17,7 +17,7 @@ function App() {
         <Section2/>
         <Section3/>
         <Section4/>
-        <FOTO_VIDEO_GALERY/>
+        {/*<FOTO_VIDEO_GALERY/>*/}
         <Section5/>
         <Footer/>
       </>}/>
