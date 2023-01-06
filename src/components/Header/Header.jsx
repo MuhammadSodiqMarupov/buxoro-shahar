b294f63f69f1993ff576b3dddf5bdf12483f7ea4
MuhammadSodiq <muhammadsodiqmarupov@gmail.com>
2023-01-06 17:05:51 +0500

    hamburger 100%

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,25 @@
-
 import "./Header.scss"
 import logo from "../../Images/logo.jpg"
 import {Dropdown} from "react-bootstrap";
 import DropdownToggle from "react-bootstrap/DropdownToggle";
 import DropdownMenu from "react-bootstrap/DropdownMenu";
 import DropdownItem from "react-bootstrap/DropdownItem";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import search from '../../Images/search.jpg';
-import {useNavigate} from "react-router-dom";
+
 function Header(props) {
-    const [langShow,setLangShow] = useState(false);
-    const navigate = useNavigate();
+    const [langShow, setLangShow] = useState(false);
+    const [hamburger, setHamburger] = useState(false);
 
     function changeLang(s) {
-        localStorage.setItem("langType",s);
+        localStorage.setItem("langType", s);
         props.refresh();
     }
 
-const menuOpen = ()=>document.querySelector(".hamburger").style.height = '100vh';
+    const menuOpen = (height) => {
+        document.querySelector(".hamburger").style.height = height
+        setHamburger(prev => !prev);
+    };
     return (
         <>
             <div className="hamburger">
@@ -26,37 +28,47 @@ const menuOpen = ()=>document.querySelector(".hamburger").style.height = '100vh'
             <div className="Header">
                 <div className="myContainer">
                     <div className="left">
-                        <img src={logo} onClick={menuOpen} width={32} height={32} alt="Logojon"/>
-                        {props.navbarData.length!=0?props.navbarData.map((item,index)=><Dropdown key={index} show={props.titles[index]} onMouseOver={()=>{
-                            props.titles[index]=true
-                            props.set([...props.titles]);
-                        }} onMouseLeave={()=>{
-                            props.titles[index]=false
-                            props.set([...props.titles]);
-                        }}>
+                        <div className={hamburger ? "left-header_active" : "left-header"}>
+                            <img src={logo} onClick={() => hamburger ? menuOpen('0vh') : menuOpen('100vh')} width={32}
+                                 height={32} alt="Logojon"/>
+                            {hamburger ?
+                                <p onClick={() => menuOpen('0')}>CLOSE</p>
+                                : ""}
+                        </div>
+                        {props.navbarData.length != 0 ? props.navbarData.map((item, index) => <Dropdown key={index}
+                                                                                                        show={props.titles[index]}
+                                                                                                        onMouseOver={() => {
+                                                                                                            props.titles[index] = true
+                                                                                                            props.set([...props.titles]);
+                                                                                                        }}
+                                                                                                        onMouseLeave={() => {
+                                                                                                            props.titles[index] = false
+                                                                                                            props.set([...props.titles]);
+                                                                                                        }}>
                             <DropdownToggle className={"myDropdown"}>
                                 {item.title}
                             </DropdownToggle>
                             <DropdownMenu>
-                                {item.items.map(item1=>
-                                    <DropdownItem>{item1.title}</DropdownItem>
+                                {item.items.map((item1, index) =>
+                                    <DropdownItem key={index}>{item1.title}</DropdownItem>
                                 )}
                             </DropdownMenu>
-                        </Dropdown>):''}
+                        </Dropdown>) : ''}
                     </div>
                     <div className="right">
                         <button className={"searchButton"}>
                             <img width={24} height={24} src={search} alt="Searchjon"/>
                         </button>
-                        <Dropdown className={"langButton"} show={langShow} onMouseLeave={()=>setLangShow(false)} onMouseOver={()=>setLangShow(true)}>
+                        <Dropdown className={"langButton"} show={langShow} onMouseLeave={() => setLangShow(false)}
+                                  onMouseOver={() => setLangShow(true)}>
                             <DropdownToggle className={"myDropdown"}>
                                 O‘Z
                             </DropdownToggle>
                             <DropdownMenu>
-                                <DropdownItem onClick={()=>changeLang('1')}>O‘Z</DropdownItem>
-                                <DropdownItem onClick={()=>changeLang('2')}>Cyrl</DropdownItem>
-                                <DropdownItem onClick={()=>changeLang('3')}>Rus</DropdownItem>
-                                <DropdownItem onClick={()=>changeLang('4')}>Eng</DropdownItem>
+                                <DropdownItem onClick={() => changeLang('1')}>O‘Z</DropdownItem>
+                                <DropdownItem onClick={() => changeLang('2')}>Cyrl</DropdownItem>
+                                <DropdownItem onClick={() => changeLang('3')}>Rus</DropdownItem>
+                                <DropdownItem onClick={() => changeLang('4')}>Eng</DropdownItem>
                             </DropdownMenu>
                         </Dropdown>
                         <button className={"murojaatButton"}>
