d77ad032e3be068dd2656a26e59b7b7e67a7e722
MuhammadYusuf <marupovmuhammadyusuf988@gmail.com>
2023-01-16 21:24:45 +0500

    yarim page

--- src/components/pages/request/Request.jsx
+++ src/components/pages/request/Request.jsx
@@ -1,10 +1,10 @@
 import "./request.scss"
-
+import spripka from "../../../Images/spripka.svg"
 function Request() {
     return (
         <div className="request">
             <div className="requestContainer">
-                <div className="card">
+                <div className="bigBox  ">
                     <h1> Buxoro hokimligiga murojaat yuborish </h1>
                     <p> Kengash uchun ariza, taklif, shikoyat va boshqa turdagi murojjatingiz bo'lsa bizga yozing. Murojaatingiz albatta hokimlik tomonidan ko'rib chiqiladi. Tashakkur! </p>
                     <div className="fio">
@@ -14,7 +14,7 @@ function Request() {
                     <div className="two_inp">
                        <div className="left">
                            <h3> Telefon* </h3>
-                           <input placeholder="+998 |" type="text"/>
+                           <input placeholder="+998 |" type="number"/>
                        </div>
                         <div className="left">
                             <h3> E-mail* </h3>
@@ -29,11 +29,11 @@ function Request() {
                         <h3> Murojaat matni* </h3>
                         <textarea placeholder="Bu yerda mavzuga oid kattagina matn bo‘ladi" name="" id="" cols="30" rows="10"></textarea>
                     </div>
-                    <div className="lastBnts">
+                    <div className="lastBtn">
                         <h3>Mavzuga oid fayl</h3>
                         <div className="btns">
-                            <button> Faylni tanlang </button>
-                            <button>  </button>
+                            <button className="btn1"><img src={spripka} alt=""/> Faylni tanlang </button>
+                            <button className="btn2"> Murojjatni yuborish </button>
                         </div>
                     </div>
                 </div>
