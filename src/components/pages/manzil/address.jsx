7eacd4a37f110572fb8da0f24e9af4bae638be05
MuhammadYusuf <marupovmuhammadyusuf988@gmail.com>
2023-01-16 21:47:09 +0500

    for test

--- src/components/pages/manzil/address.jsx
+++ src/components/pages/manzil/address.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './address.scss';
 import DynamicSideBar from '../../SideBar/DynamicSideBar';
+import viloyatHokim from "../../../Images/viloyathokim.svg"
 const Address = (props) => {
     return (
         <div className='address'>
-            <div className="container">
-                <DynamicSideBar setPage={props.set} object={props.data}/>
+            <div className="AddressContainer">
+                <DynamicSideBar object={props.data}/>
                 <div className="address-content">
-                    
+                    <div className="cardChiefCard">
+                        <div className="left">
+                            <img src={viloyatHokim} alt=""/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
