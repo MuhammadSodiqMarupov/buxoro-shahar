e7afd16a01d11e4137cfbfeaee3a05ab4e240bbb
MuhammadSodiq <muhammadsodiqmarupov@gmail.com>
2023-01-17 19:27:57 +0500

    sideBar tayyorlandi

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ import Request from "./components/pages/request/Request";
 import {useLocation} from "react-router-dom";
 import Malumot from "./components/pages/shahar-haqida/malumot/Malumot";
 import Deparments from "./components/pages/shahar-haqida/reginal_departments/Deparments";
+import Surovnoma from "./components/pages/ijtimoiy_sorovnoma/Surovnoma";
+import Savollar from "./components/pages/savollar/Savollar";
+import Hotels from "./components/pages/hotels/Hotels";
+import Agency from "./components/pages/agency/Agency";
 const Header = React.lazy(()=>import('./components/Header/Header'))
 const Navbar = React.lazy(()=>import('./components/Navbar/Navbar'))
 const Section2 = React.lazy(()=>import('./components/Section2/Section2'))
@@ -130,6 +134,10 @@ function App() {
       <Route path={"/request"} element={<Request/>}/>
       <Route path="/information-about-the-province" element={<Malumot data={getDataFilter("information-about-the-province")}/>}/>
       <Route path="/regionaldepartments" element={<Deparments data={getDataFilter("regionaldepartments")}/>}/>
+      <Route path="/answers-to-frequently-asked-questions" element={<Surovnoma data={getDataFilter("answers-to-frequently-asked-questions")}/>}/>
+      <Route path="/social-questionnaire" element={<Savollar data={getDataFilter("social-questionnaire")}/>}/>
+      <Route path="/hotels" element={<Hotels data={getDataFilter("hotels")}/>}/>
+      <Route path="/travel-agencies" element={<Agency data={getDataFilter("travel-agencies")}/>}/>
       </Route>
       </Routes>
   );
