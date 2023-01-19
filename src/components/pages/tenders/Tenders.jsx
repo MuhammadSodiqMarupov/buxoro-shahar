46c3fbb1381b0425f2258eb808ed60ef14628d5a
MuhammadYusuf <marupovmuhammadyusuf988@gmail.com>
2023-01-19 19:16:23 +0500

    elonlar

--- src/components/pages/tenders/Tenders.jsx
+++ src/components/pages/tenders/Tenders.jsx
@@ -1,13 +1,26 @@
 import DynamicSideBar from '../../SideBar/DynamicSideBar';
 import './tenders.scss';
-const Tenders = (props) => {
+import duduuuk from "../../../Images/dukduk.svg"
+const Tenders = ({arr,data}) => {
     return (
-        <div className='tenders'>
+        <div className="tenders">
             <div className="tenders__container">
-                <DynamicSideBar object={props.data}/>
-            </div>
+                <DynamicSideBar object={data}/>
             <div className="content">
-                
+                <h1> E’lonlar va tenderlar </h1>
+                <div className="card_content">
+                    {arr.map(item=><div className="cardTender">
+                        <div className="topCard">
+                            <img src={duduuuk} alt=""/>
+                            <h3> {item.title}</h3>
+                        </div>
+                        <div className="bottomDuduk">
+                            <p> {item.berifDescription}</p>
+                            <button className="date">{item.date}</button>
+                        </div>`
+                    </div>)}
+                </div>
+            </div>
             </div>
         </div>
     );
